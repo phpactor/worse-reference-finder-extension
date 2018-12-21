53c0a1760c980bf59801460532015c26152a6598
Daniel Leech <dan.t.leech@gmail.com>
2018-12-21 10:38:40 +0000

    initial

---
+++ lib/WorseReferenceFinderExtension.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Phpactor\Extension\WorseReferenceFinder;
+
+use Phpactor\Container\Container;
+use Phpactor\Container\ContainerBuilder;
+use Phpactor\Container\Extension;
+use Phpactor\Extension\ReferenceFinder\ReferenceFinderExtension;
+use Phpactor\Extension\WorseReflection\WorseReflectionExtension;
+use Phpactor\MapResolver\Resolver;
+use Phpactor\WorseReferenceFinder\WorsePlainTextClassDefinitionLocator;
+use Phpactor\WorseReferenceFinder\WorseReflectionDefinitionLocator;
+
+class WorseReferenceFinderExtension implements Extension
+{
+    const PARAM_BREAK_CHARS = 'worse_reference_finder.plain_text_break_chars';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ContainerBuilder $container)
+    {
+        $container->register('worse_reference_finder.definition_locator.reflection', function (Container $container) {
+            return new WorseReflectionDefinitionLocator(
+                $container->get(WorseReflectionExtension::SERVICE_REFLECTOR)
+            );
+        }, [ ReferenceFinderExtension::TAG_DEFINITION_LOCATOR => []]);
+
+        $container->register('worse_reference_finder.definition_locator.plain_text_class', function (Container $container) {
+            return new WorsePlainTextClassDefinitionLocator(
+                $container->get(WorseReflectionExtension::SERVICE_REFLECTOR),
+                $container->getParameter(self::PARAM_BREAK_CHARS)
+            );
+        }, [ ReferenceFinderExtension::TAG_DEFINITION_LOCATOR => []]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function configure(Resolver $schema)
+    {
+        $schema->setDefaults([
+            self::PARAM_BREAK_CHARS => [' ', '"', '\'', '|', '%', '(', ')', '[', ']',':']
+        ]);
+        $schema->setTypes([
+            self::PARAM_BREAK_CHARS => 'array',
+        ]);
+    }
+}
---
+++ tests/Unit/WorseReferenceFinderExtensionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Phpactor\Extension\WorseReferenceFinder\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Phpactor\Container\PhpactorContainer;
+use Phpactor\Extension\ClassToFile\ClassToFileExtension;
+use Phpactor\Extension\ComposerAutoloader\ComposerAutoloaderExtension;
+use Phpactor\Extension\Logger\LoggingExtension;
+use Phpactor\Extension\ReferenceFinder\ReferenceFinderExtension;
+use Phpactor\Extension\WorseReferenceFinder\WorseReferenceFinderExtension;
+use Phpactor\Extension\WorseReflection\WorseReflectionExtension;
+use Phpactor\FilePathResolverExtension\FilePathResolverExtension;
+use Phpactor\ReferenceFinder\DefinitionLocator;
+use Phpactor\TextDocument\ByteOffset;
+use Phpactor\TextDocument\TextDocumentBuilder;
+
+class WorseReferenceFinderExtensionTest extends TestCase
+{
+    public function testRegistration()
+    {
+        $container = PhpactorContainer::fromExtensions([
+            WorseReferenceFinderExtension::class,
+            WorseReflectionExtension::class,
+            ReferenceFinderExtension::class,
+            FilePathResolverExtension::class,
+            ClassToFileExtension::class,
+            ComposerAutoloaderExtension::class,
+            LoggingExtension::class,
+        ], [
+            'file_path_resolver.application_root' => __DIR__ . '/..',
+        ]);
+
+        $locator = $container->get(ReferenceFinderExtension::SERVICE_DEFINITION_LOCATOR);
+
+        assert($locator instanceof DefinitionLocator);
+
+        $location = $locator->locateDefinition(
+            TextDocumentBuilder::create(WorseReferenceFinderExtension::class)->build(),
+            ByteOffset::fromInt(3)
+        );
+
+        $this->assertEquals(realpath(__DIR__ . '/../../lib/WorseReferenceFinderExtension.php'), $location->uri());
+    }
+}
