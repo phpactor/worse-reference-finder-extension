758d313f91e7303bc3b7ae3795b0259e07ead608
Daniel Leech <dan.t.leech@gmail.com>
2020-01-26 21:15:05 +0000

    Remove broken implementation finder

--- lib/WorseReferenceFinderExtension.php
+++ lib/WorseReferenceFinderExtension.php
@@ -6,21 +6,14 @@ use Phpactor\Container\Container;
 use Phpactor\Container\ContainerBuilder;
 use Phpactor\Container\Extension;
 use Phpactor\Extension\ReferenceFinder\ReferenceFinderExtension;
-use Phpactor\Extension\SourceCodeFilesystem\SourceCodeFilesystemExtension;
 use Phpactor\Extension\WorseReflection\WorseReflectionExtension;
 use Phpactor\MapResolver\Resolver;
-use Phpactor\WorseReferenceFinder\SourceCodeFilesystem\SimilarityPreFilter\AbstractnessFilter;
-use Phpactor\WorseReferenceFinder\SourceCodeFilesystem\SimilarityPreFilter\ChainFilter;
-use Phpactor\WorseReferenceFinder\SourceCodeFilesystem\SimilarityPreFilter\PathFqnSimilarityFilter;
-use Phpactor\WorseReferenceFinder\SourceCodeFilesystem\WorseClassImplementationFinder;
 use Phpactor\WorseReferenceFinder\WorsePlainTextClassDefinitionLocator;
 use Phpactor\WorseReferenceFinder\WorseReflectionDefinitionLocator;
 
 class WorseReferenceFinderExtension implements Extension
 {
     const PARAM_BREAK_CHARS = 'worse_reference_finder.plain_text_break_chars';
-    const PARAM_ENABLE_SIMILARITY_FILTER = 'worse_reference_finder.implementation_finder.similarity_filter';
-    const PARAM_ENABLE_ABSTRACTNESS_FILTER = 'worse_reference_finder.implementation_finder.abstractness_filter';
 
     /**
      * {@inheritDoc}
@@ -39,24 +32,6 @@ class WorseReferenceFinderExtension implements Extension
                 $container->getParameter(self::PARAM_BREAK_CHARS)
             );
         }, [ ReferenceFinderExtension::TAG_DEFINITION_LOCATOR => []]);
-
-        $container->register('worse_reference_finder.implementation_finder.worse', function (Container $container) {
-            $filters = [];
-
-            if ($container->getParameter(self::PARAM_ENABLE_ABSTRACTNESS_FILTER)) {
-                $filters[] = new AbstractnessFilter();
-            }
-
-            if ($container->getParameter(self::PARAM_ENABLE_SIMILARITY_FILTER)) {
-                $filters[] = new PathFqnSimilarityFilter();
-            }
-
-            return new WorseClassImplementationFinder(
-                $container->get(WorseReflectionExtension::SERVICE_REFLECTOR),
-                $container->get(SourceCodeFilesystemExtension::SERVICE_FILESYSTEM_COMPOSER),
-                new ChainFilter(...$filters)
-            );
-        }, [ ReferenceFinderExtension::TAG_IMPLEMENTATION_FINDER=> []]);
     }
 
     /**
@@ -65,8 +40,6 @@ class WorseReferenceFinderExtension implements Extension
     public function configure(Resolver $schema)
     {
         $schema->setDefaults([
-            self::PARAM_ENABLE_ABSTRACTNESS_FILTER => true,
-            self::PARAM_ENABLE_SIMILARITY_FILTER => true,
             self::PARAM_BREAK_CHARS => [' ', '"', '\'', '|', '%', '(', ')', '[', ']',':',"\r\n", "\n", "\r"]
         ]);
         $schema->setTypes([
